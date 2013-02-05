1977998e2403c991eb689580b3158dcfdd56a45c
Yoan Roullard <yoan@yoan-Satellite-P300>
2013-02-05 09:27:14 +0100

    initial commit

---
+++ src/main/java/yroul/flickexplorer/App.java
@@ -0,0 +1,13 @@
+package yroul.flickexplorer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
